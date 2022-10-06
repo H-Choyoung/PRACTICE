7cc3fef3e8585c24bcb35749912e4c07583f8844
Choyoung <lll0858630@gmail.com>
2022-10-06 17:00:53 +0900

    issue23 끝

--- work/issue23.js
+++ work/issue23.js
@@ -1,7 +1,25 @@
-
-    const issue23 = "work";
-
-    module.exports = {
-      issue22
-    };
-  
+    const pokemon = require('pokemon');
+    
+    class Pokemon46 {
+      constructor(id, name, koreaName) {
+        this.id = id;
+        this.name = name;
+        this.koreaName = koreaName;
+      }
+    }
+    function pokemonObj(data) {
+      const int = [];
+      let findNum = 23*2; //issue 23*2
+      let temp = new Pokemon46(findNum, data.getName(findNum), data.getName(findNum, "ko"));
+      int.push(temp);
+      return int;
+    }
+    
+    // console.log(pokemon.all()); //포켓몬들의 이름 출력됨
+    // console.log(pokemon);
+    // console.log(pokemon.all().length); //905
+    // console.log(pokemon.getName());
+    // console.log(pokemonObj(pokemon));
+    
+    const issue23 = pokemonObj(pokemon);
+    console.log(issue23); //[ Pokemon46 { id: 46, name: 'Paras', koreaName: '파라스' } ]
